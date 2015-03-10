62191e163f37a23407246702abdaff2c81f31b4a
John Wileczek <john.wileczek@gmail.com>
2015-03-10 19:35:15 -0400

    Made changes to the build script so it would run on VM. Created a first pass controller to initialize values and calculate X,Y,Z Values.

--- src/common/calc/CalcDirective.js
+++ src/common/calc/CalcDirective.js
@@ -1,7 +1,22 @@
 (function() {
   var module = angular.module('calc_directive', []);
 
-  module.directive('calc', function(calcService, $rootScope) {
+  module.controller('CalcController',function(calcService,$scope,$window) {
+    $scope.ageAtHire = 25;
+    $scope.ageAtRetire = 55;
+    $scope.wageAtHire = 20000;
+    $scope.wageAtRetire = 62374;
+    $scope.finalSalaryYears = 3;
+    $scope.COLAAdjustment = 2;
+    $scope.definedContribution = 33.62;
+    $scope.wageIncrease = 0.04;
+    $scope.investReturn = 0.06;
+    $scope.xValue = 0;
+    $scope.xValue = calcService.ComputeXValue($scope.ageAtHire,$scope.ageAtRetire,$scope.wageAtHire,$scope.definedContribution,$scope.investReturn,$scope.wageIncrease);
+    
+  });
+  
+  /*module.directive('calc', function(calcService, $rootScope) {
     return {
       restrict: 'C',
       replace: false,
@@ -9,20 +24,7 @@
 
       // The linking function will add behavior to the template
       link: function(scope, element) {
-        scope.apps = null;
-
-        scope.getApps = function() {
-          // search my contacts
-          calcService.getApps().then(function(apps) {
-            scope.apps = apps;
-          }, function(reject) {
-            console.log('---- calcService.getApps error: ', reject);
-            scope.apps = null;
-          });
-        };
-
-        scope.getApps();
+        
       }
-    };
-  });
+    };*/
 }());
--- src/common/calc/CalcService.js
+++ src/common/calc/CalcService.js
@@ -3,74 +3,159 @@
 
   //var service_ = null;
   var q_ = null;
-  var apps_ = null;
 
   module.provider('calcService', function() {
     this.$get = function($rootScope, $q) {
       q_ = $q;
       return this;
     };
-
-    this.getApps = function() {
-      var deferredResponse = q_.defer();
-
-      //TODO: get list of apps by asking the server to get a list of apps by in turn hitting the admin apps page as an admin
-      apps_ = [];
-
-      apps_.push({
-        name: 'Drive',
-        link: 'https://drive.google.com/?authuser=0',
-        icon: '/assets/images/drive-64.png'
-      });
-
-      apps_.push({
-        name: 'Mail',
-        link: 'https://mail.google.com/mail/?authuser=0',
-        icon: '/assets/images/googlemail-64.png'
-      });
-
-      apps_.push({
-        name: 'Calendar',
-        link: 'https://www.google.com/calendar?authuser=0',
-        icon: '/assets/images/calendar-64.png'
-      });
-
-      apps_.push({
-        name: 'Contacts SB',
-        link: 'https://smartbook-contacts.appspot.com?authuser=0',
-        icon: '/assets/images/planner-64.png'
-      });
-
-      apps_.push({
-        name: 'Start Hangout',
-        link: 'https://hangouts.google.com/start',
-        icon: '/assets/images/hangouts-64.png'
-      });
-
-      //TODO: link to training material should be configurable
-      apps_.push({
-        name: 'Training',
-        link: 'https://drive.google.com/a/findplango.com/folderview?id=0ByrOVPg5iAJofkYzNFhrNDBNcW94M0xJOWlSNkRTelVQcXV1djNNTzBoR0xESzczaXc2VW8&usp=sharing',
-        icon: '/assets/images/play_books-64.png'
-      });
-
-      apps_.push({
-        name: 'Voice',
-        link: 'https://www.google.com/voice/b/0?pli=1#inbox',
-        icon: '/assets/images/voice-64.png'
-      });
-
-      apps_.push({
-        name: 'Maps',
-        link: 'https://www.google.com/maps',
-        icon: '/assets/images/maps-64.png'
-      });
-
-      // resolve as if we got all the apps
-      deferredResponse.resolve(apps_);
-
-      return deferredResponse.promise;
+    
+    this.ComputeXValue = function(ageAtHire,ageAtRetire,wageAtHire,definedContribution,investReturn,wageIncrease) {
+        var xValue;
+        xValue = ((wageAtHire * definedContribution) / (investReturn - wageIncrease)) * (pow(1 - ((1 + wageIncrease)/(1 + investReturn)),ageAtRetire-ageAtHire));
+        return xValue;
     };
-
+    
+    this.ComputeYValue = function(ageAtHire,ageAtRetire,wageAtHire,definedContribution,investReturn) {
+        var yValue;
+        yValue = (pow((1 + investReturn),ageAtRetire - ageAtHire) * (1 + (investReturn*11/24)));
+        return yValue;
+    };
+    
+    
+    
+    this.ComputeZValue = function(rates, rateStructure, mortName, mortTable, mortProjection, age, ARA, sex, certainPeriod, tempPeriod, spouseAge, pctEE, pctBoth, pctSpouse, COLApct, COLAStartAge) {
+      var zValue = 0;
+      var x,y,z = 0;    //counters for something
+      var spouseARA = 0;
+      var minJSq, maxJSq; //joint mortality table min and max
+      var a_ee, a_sp, a_eesp = 0;
+      var p_ee = [];
+      p_ee[age] = 1;
+      var p_sp = [];
+      var p_eesp = [];
+      p_eesp[age] = 1;
+      var qn_m = [];    //q for non-annuitant male
+      var qn_f = [];    //q for non-annuitant female
+      var qa_m = [];    //q for annuitant male
+      var qa_f = [];    //q for annuitant female
+      var q417n_m = []; //'q for non-annuitant male 417(e)
+      var q417n_f = []; //'q for non-annuitant female 417(e)
+      var q417a_m = []; //'q for annuitant male 417(e)
+      var q417a_f = []; //'q for annuitant female 417(e)
+      var AA_m = [];    //'Scale AA for males
+      var AA_f = [];    //'Scale AA for Females
+      var q_m = [];     //'q for males
+      var q_f = [];     //'q for females
+      var q417_m = [];  //'q for males 417(e)
+      var q417_f = [];  //'q for females 417(e)
+      var q_ee = [];    //'q for employee in a J&S calculation
+      var q_sp = [];    //'q for spouse in a J&S calculation
+      var q417 = [];    //'417(e) mortality table
+      var q_eesp = [];  //'q_ee and sp for J&S calculation
+      var Weight_m = [];
+      var Weight_f = [];
+      var qUP94_m = [];
+      var qUP94_f = [];
+      var qGar94 = [];
+      var qGam83_m = [];
+      var qGam83_f = [];
+      var DiscountValue_ee = [];
+      var DiscountValue_sp = [];
+      var DiscountValue_eesp = [];
+      var AdjDiscountValue_ee = [];
+      var AdjDiscountValue_sp = [];
+      var AdjDiscountValue_eesp = [];
+      var Interest = [];
+      var pvCertainPeriod = 0;
+      var MonthlyRate = 0;
+      var CountRates = 0;
+      var Startage = 0; //'for mortality table
+      var EndAge;       //'for mortality table
+      var v = [];       //'interest discount for forward rates
+      var COLAincrease = [];
+      
+      //Input validation
+      if(ARA < age) {
+        certainPeriod = max(certainPeriod - age + ARA, 0);
+        tempPeriod = max(tempPeriod - age + ARA, 0);
+        ARA = age;
+      }
+      
+      //Set mortality table start age and end age
+      if(mortName == "ownstatic" || mortName == "owngenerational") {
+        Startage = mortTable[1][1];
+        EndAge = min(mortTable.length + Startage - 1, 120);
+      } 
+      else {
+        Startage = 1;
+        EndAge = 120;
+      }
+      
+      //Setup spouse information
+      if(spouseAge < 1) {
+        spouseAge = age;
+        spouseARA = ARA;
+        pctEE = 1;
+        pctBoth = 1;
+        pctSpouse = 0;
+      } 
+      else {
+        spouseARA = ARA - age + spouseAge;
+      }
+      p_sp[spouseAge] = 1;
+      
+      //Set up interest array
+      for(x = 1; x < age - 1; x++) {
+        interest[x] = 0;
+      }
+      
+      if(rateStructure == "spot") {
+        numRates = rates.length;
+        if(numRates < 3) {
+          for(x = age; x < age + 4; x++) {
+            interest[x] = rates[1];
+          }
+          
+          for(x = age + 5; x < age + 19; x++) {
+            interest[x] = rates[2];
+          }
+          
+          for(x = age + 20; x < 200; x++) {
+            interest[x] = rates[3];
+          }
+        } 
+        else {
+          for(x = age; x < age + numRates - 1; x++) {
+            interest[x] = rates[x - age + 1];
+          }
+          for(x = age + numRates - 1; x < 200; x++) {
+            interest[x] = rates[numRates];
+          }
+        }
+     }
+     var invalidRates = false;
+      if(rateStructure == "forward") {
+        //Check typename of rates
+        if(rates.length > 1) {
+          z = 0;
+          for(y = 1; y < rates.length; y++) {
+            for(x = age + z; x < age + rates[y][1] - 1; x++) {
+                interest[x] = rates[y][2];
+            }
+            z = rates[y][1];
+          }
+        }
+        if(invalidRates) {
+            z = 0;
+            for(y = 1; y < rates.length; y+=2) {
+                for(x = age + z; x < age + rates[y] - 1; x++) {
+                    interest[x] = rates[y+1];
+                }
+                z = rates[y];
+            }
+        }
+    }
+};
   });
 }());

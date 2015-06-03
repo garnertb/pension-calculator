74b1e02e6893f5ea9411bcc3096cc7226a182248
Syrus Mesdaghi <syrusm@gmail.com>
2015-06-02 21:20:12 -0400

    removed files that are no longer used

--- src/common/calc/CalcModule.js
+++ src/common/calc/CalcModule.js
@@ -1,9 +1,6 @@
 (function() {
   angular.module('calc', [
     'calc_service',
-    'calc_directive',
-    'defined_contribution_directive',
-    'defined_benefit_directive',
-    'benefit_reduction_directive'
+    'calc_directive'
   ]);
 }());

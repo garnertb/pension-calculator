6ddf53b86b8a6cd08bd6cff15d53f9bed836b225
Tyler Garner <garnertb@gmail.com>
2015-07-09 13:37:21 -0400

    Test enabling html5 mode.

--- src/app/app.js
+++ src/app/app.js
@@ -6,9 +6,16 @@ angular.module('benefitEquivalentCalculator', [
   'calc'
 ]).config(function myAppConfig() {
   console.log('---- AppCtrl.config');
+}).config(function($locationProvider) {
+  //Without this the pension calculator will append "/" to the # in the IAFF routing
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+    rewriteLinks: false
+  });
 }).run(function run() {
   console.log('---- AppCtrl.run');
-}).controller('AppCtrl', function AppCtrl($scope, $location, calcService) {
+}).controller('AppCtrl', function AppCtrl($scope, calcService) {
   console.log('---- AppCtrl');
   $scope.pageTitle = 'Benefit Equivalent Calculator';
   calcService.ComputeXValue();
